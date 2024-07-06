292f029c347e61fa2812c4640322ec821872012e
Rylan Sedlacek <99748019+rylansedlacek@users.noreply.github.com>
2024-07-06 16:09:35 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,20 +2,20 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import gsap from 'gsap';
 
-// Initialize AOS (Animate On Scroll)
+
 AOS.init({
   duration: 1200,
   once: true,
 });
 
-// GSAP Animation
+
 gsap.from('.hero-content', {
   opacity: 0,
   duration: 2,
   y: -50,
 });
 
-// Simple example: Log a message when the document is loaded
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Document loaded and ready.');
   });
